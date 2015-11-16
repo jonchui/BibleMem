ae888bc1d570f4876b594143e1436d1602e2df5a
jonchui <jonchui@google.com>
2015-11-15 16:37:49 -0800

    cannot find VerseCEll indeppath, weird.

--- iOS/BibleMem_ObjC/DetailViewController.m
+++ iOS/BibleMem_ObjC/DetailViewController.m
@@ -50,11 +50,59 @@
   [self.tableView reloadData];
 }
 
+#pragma mark - Table View
+
+- (BOOL) hasDownloadedVerseData {
+  return [self.verses count] > 0;
+}
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+  return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+  return [self hasDownloadedVerseData] ? [self.verses count] : 1;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+  NSString *identifier = [self hasDownloadedVerseData] ? @"VerseCell" : @"LoadingCell";
+  UITableViewCell *cell =
+      [tableView dequeueReusableCellWithIdentifier:identifier forIndexPath:indexPath];
+  [self configureCell:cell atIndexPath:indexPath];
+  return cell;
+}
+
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+  // Return NO if you do not want the specified item to be editable.
+  return NO;
+}
+
+- (void)configureCell:(UITableViewCell *)cell atIndexPath:(NSIndexPath *)indexPath {
+  
+  if ([self hasDownloadedVerseData]) {
+    DBTVerse *verse = (DBTVerse *)[self.verses objectAtIndex:indexPath.row];
+    cell.textLabel.text = verse.verseText;
+    UITextView *textView = (UITextView *)[cell.contentView viewWithTag:100];
+    if (!textView) {
+      textView.text = verse.verseText;
+    }
+  } else {
+    cell.textLabel.text = @"Downloading... ";
+  }
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+
+}
+
+#pragma mark - system view stuff
+
 - (void)viewDidLoad {
   [super viewDidLoad];
   // Do any additional setup after loading the view, typically from a nib.
   [self configureView];
 }
+
+#pragma mark - memory
 
 - (void)didReceiveMemoryWarning {
   [super didReceiveMemoryWarning];
